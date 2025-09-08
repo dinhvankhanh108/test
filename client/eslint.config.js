6310cb092450e6019860d2b32463232a6bcdf645
Giang <1551010050khanh@ou.edu.vn>
2025-09-08 16:02:56 +0700

    config eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -13,21 +13,19 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'comma-dangle': ['error', 'never'],
+      'indent': ['error', 2],
+      'semi': ['error', 'never'],
+      'no-unused-vars': 'off',
+      'quotes': ['error', 'single']
+    }
+  }
 ]
